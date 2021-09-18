fcbb38e2870810ca2ebb12818b4b870a7d198dde
dominikus1993 <dominikus1910@gmail.com>
2021-09-18 09:45:13 +0000

    subprocess

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import git
+import subprocess
 
-repo = git.Repo()
-commit_dev = repo.commit("df984b8fded74f477bbdb366d3b61b3daa741a1a")
-commit_origin_dev = repo.commit("HEAD")
-diff_index = commit_origin_dev.diff(commit_dev)
+git = subprocess.run(["git", 'diff', "dadd78463431798db69b1452a7e8c159743cc7ed", 'HEAD', "--color"])
 
-for diff_item in diff_index.iter_change_type('M'):
-    print("A blob:\n{}".format(diff_item.a_blob.data_stream.read().decode('utf-8')))
-    print("B blob:\n{}".format(diff_item.b_blob.data_stream.read().decode('utf-8')))
+print(git.stdout.decode('utf-8'))
+
