9f5b21ce832c1057a4441df45d36cf7b0bd4c9ea
dominikus1993 <dominikus1910@gmail.com>
2021-09-18 15:09:15 +0000

    small chnages

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import subprocess
+from ansi2html import Ansi2HTMLConverter
 
-git = subprocess.run(["git", 'diff', "dadd78463431798db69b1452a7e8c159743cc7ed", 'HEAD', "--color"])
+git = subprocess.run(["git", 'diff', "dadd78463431798db69b1452a7e8c159743cc7ed", 'HEAD', "--color"], capture_output=True, shell=False)
 
-print(git.stdout.decode('utf-8'))
+conv = Ansi2HTMLConverter()
 
+ansi = git.stdout.decode('utf-8')
+
+html = conv.convert(ansi)
+
+f = open("xd.html", "w")
+f.write(html)
