5c6ea327c4f65724ec6cc6a71e5a7ce796ff9fdd
dominikus1993 <dominikus1910@gmail.com>
2021-09-18 06:37:22 +0000

    small changes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import git
 
-
 repo = git.Repo()
+commit_dev = repo.commit("df984b8fded74f477bbdb366d3b61b3daa741a1a")
+commit_origin_dev = repo.commit("HEAD")
+diff_index = commit_origin_dev.diff(commit_dev)
+
+for diff_item in diff_index.iter_change_type('M'):
+    print("A blob:\n{}".format(diff_item.a_blob.data_stream.read().decode('utf-8')))
+    print("B blob:\n{}".format(diff_item.b_blob.data_stream.read().decode('utf-8')))
