11d397a30dacb851d77fd6ac042b84c070957adc
dominikus1993 <dominikus1910@gmail.com>
2021-09-18 15:39:36 +0000

    add script

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import subprocess
 from ansi2html import Ansi2HTMLConverter
+import argparse
 
-git = subprocess.run(["git", 'diff', "dadd78463431798db69b1452a7e8c159743cc7ed", 'HEAD', "--color"], capture_output=True, shell=False)
 
-conv = Ansi2HTMLConverter()
+parser = argparse.ArgumentParser(description='Create diff from two commits.')
 
-ansi = git.stdout.decode('utf-8')
+parser.add_argument('--path', metavar='N', default="./", type=str, help='string')
+parser.add_argument('--commit', metavar='N', default="HEAD", type=str, help='string')
+parser.add_argument('--commit2diff', metavar='N', type=str, help='string')                    
+parser.add_argument('--htmlpath', metavar='N', default="diff.html", type=str, help='string')   
 
-html = conv.convert(ansi)
+args = parser.parse_args()
 
-f = open("xd.html", "w")
-f.write(html)
+def get_diff(path: str = "./", commit: str = "HEAD", commit2diff: str = "HEAD^") -> str:
+    git = subprocess.run(["git", 'diff', commit2diff, commit, "--color"], capture_output=True, shell=False, cwd=path)
+    return git.stdout.decode('utf-8')
+
+def save_diff_as_html(text: str, html_path: str):
+    converter = Ansi2HTMLConverter()
+    html = converter.convert(text)
+    with open(html_path, "w") as f:
+        f.write(html)
+
+diff = get_diff(args.path, args.commit, args.commit2diff)
+
+save_diff_as_html(diff, args.htmlpath)
