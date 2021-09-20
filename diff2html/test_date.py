e20ee9d31511cfdd63a173a096919c6347244eb2
dominikus1993 <dominikus1910@gmail.com>
2021-09-20 18:10:28 +0000

    small changes

--- diff2html/test_date.py
+++ diff2html/test_date.py
@@ -1,9 +1,9 @@
 
 from datetime import datetime
-from app.date import get_first_day_of_month
+from diff2html.date import get_first_day_of_month
 import unittest
 
 class TestDate(unittest.TestCase):
         def test_first_day(self):
-                subject = get_first_day_of_month(datetime.strptime("2021-09-31", '%Y-%m-%d'))
+                subject = get_first_day_of_month(datetime.strptime("2021-09-30", '%Y-%m-%d'))
                 self.assertEqual(subject, "2021-09-31")
