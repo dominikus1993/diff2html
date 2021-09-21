ca1f4eefe46f83c6068b0f6eb476d8662161e0c5
Dominik Kotecki <dominik.kotecki@rossmann.com.pl>
2021-09-21 09:16:46 +0200

    fix date

--- diff2html/date.py
+++ diff2html/date.py
@@ -1,15 +1,16 @@
-
 from typing import Union
-from datetime import datetime
+import datetime
+import calendar
 
 def get_first_day_of_month(date: datetime) -> str:
-    return str(date.replace(day=1))
+    return str(date.replace(day=1).date())
 
 def get_first_day_of_month_when_none(date: Union[str, None]) -> str:
-    return get_first_day_of_month(datetime.today()) if date is None else date
+    return get_first_day_of_month(datetime.date.today()) if date is None else date
 
-def get_last_day_of_month(date: datetime) -> str:
-    return str(date.replace(day=1))
+def get_last_day_of_month(d: datetime) -> str:
+    date = datetime.date(d.year, d.month, calendar.monthrange(d.year, d.month)[-1])
+    return str(date)
 
 def get_last_day_of_month_when_none(date: Union[str, None]) -> str:
-    return get_first_day_of_month(datetime.today()) if date is None else date
+    return get_first_day_of_month(datetime.date.today()) if date is None else date
--- diff2html/test_date.py
+++ diff2html/test_date.py
@@ -1,9 +1,12 @@
 
 from datetime import datetime
-from diff2html.date import get_first_day_of_month
+from diff2html.date import get_first_day_of_month, get_last_day_of_month
 import unittest
 
 class TestDate(unittest.TestCase):
         def test_first_day(self):
                 subject = get_first_day_of_month(datetime.strptime("2021-09-30", '%Y-%m-%d'))
-                self.assertEqual(subject, "2021-09-31")
+                self.assertEqual(subject, "2021-09-01")
+        def test_last_day(self):
+                subject = get_last_day_of_month(datetime.strptime("2021-09-15", '%Y-%m-%d'))
+                self.assertEqual(subject, "2021-09-30")
