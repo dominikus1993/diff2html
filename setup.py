1297230364ef692875bb90ff99ed08930fe06491
Dominik.Kotecki <dominikus1910@gmail.com>
2022-03-29 14:58:03 +0000

    update packages

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import os
 from setuptools import setup, find_packages
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
