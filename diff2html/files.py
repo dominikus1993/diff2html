b8b850f4bfb281893cf8d3365e1010408e4661f4
Dominik.Kotecki <dominikus1910@gmail.com>
2023-01-11 18:40:30 +0000

    fix zip file writer

--- diff2html/files.py
+++ diff2html/files.py
@@ -14,5 +14,5 @@ def write_force(path: str, content: str):
 def write_zip_force(path: str, content_path: str):
     if os.path.exists(path=path):
         os.remove(path=path)
-    with zipfile.ZipFile(path, 'a') as zf:
-        zf.write(path, os.path.basename(content_path))
+    with zipfile.ZipFile(path, 'w') as zf:
+        zf.write(os.path.basename(content_path))
--- diff2html/output.py
+++ diff2html/output.py
@@ -8,24 +8,11 @@ from ansi2html import Ansi2HTMLConverter
 
 from diff2html.files import write_force, write_zip_force
 
-
 class DiffWriter(abc.ABC):
 
     @abc.abstractmethod
     def write(self, diff: str, filename: str) -> None:
         pass
-
-
-class ZipDiffWriter(DiffWriter):
-
-    def write(self, diff: str, filename: str) -> None:
-        html_path = f'{filename}.html'
-        zip_path = f'{filename}.zip'
-        converter = Ansi2HTMLConverter()
-        html = converter.convert(diff)
-        write_force(html_path, html)
-        write_zip_force(zip_path, html_path)
-        os.remove(html_path)
 
 class HtmlDiffWriter(DiffWriter):
     
@@ -35,9 +22,24 @@ class HtmlDiffWriter(DiffWriter):
         html = converter.convert(diff)
         write_force(html_path, html)
 
+class ZipDiffWriter(DiffWriter):
+
+    htmL_writer: HtmlDiffWriter
+    
+    def __init__(self, htmL_writer: HtmlDiffWriter) -> None:
+        self.htmL_writer = htmL_writer
+        super().__init__()
+    
+    def write(self, diff: str, filename: str) -> None:
+        html_path = f'{filename}.html'
+        zip_path = f'{filename}.zip'
+        self.htmL_writer.write(diff, filename)
+        write_zip_force(zip_path, html_path)
+
+
 def get_writer(output_type: str) -> DiffWriter:
     if output_type == "zip":
-        return ZipDiffWriter()
+        return ZipDiffWriter(HtmlDiffWriter())
     elif output_type == "html":
         return HtmlDiffWriter()
     raise ValueError(f"Unknown output type: {output_type}")
